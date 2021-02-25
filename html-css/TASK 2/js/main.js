0795c401b2805e025de8e7daabbbb6339107f426
Annie <btwhoway@gmail.com>
2021-02-25 22:46:55 +0200

    checkout icon, modal window. form validations

--- html-css/TASK 2/js/main.js
+++ html-css/TASK 2/js/main.js
@@ -46,7 +46,17 @@ let tours = [
     }
 ]
 
+let startFlag = false;
+
 document.querySelector("#start").addEventListener("click", () => {
+    if (startFlag) return;
+    startFlag = true;
+    let title = document.createElement("p");
+    title.innerHTML = 'Choose your tour now!';
+    document.querySelector("#choose").appendChild(title);
+    let info = document.createElement("div");
+    info.id = "tours";
+
     tours.forEach(tour => {
         let box = document.createElement("div");
         box.classList.add("tour-box");
@@ -71,10 +81,124 @@ document.querySelector("#start").addEventListener("click", () => {
         box.appendChild(age);
 
         let btn = document.createElement("button");
-        btn.innerHTML = "INFO";
-        box.appendChild(btn)
-        
-        document.querySelector("#tours").appendChild(box);
+        btn.innerHTML = "BOOK";
+        box.appendChild(btn);
+        btn.addEventListener("click", () => {
+            let checkout = document.createElement("div");
+            checkout.classList.add("checkout");
+            checkout.innerHTML = '<i class="fas fa-route"></i>';
+            box.appendChild(checkout);
+
+            checkout.addEventListener("click", () => {
+                let modalback = document.createElement("div");
+                modalback.classList.add("modal-back");
+                checkout.hidden = true;
+                let checkoutInfo = document.createElement("div");
+                checkoutInfo.classList.add("checkout-box");
+
+                let imgdiv = document.createElement("div");
+                let img = document.createElement("img");
+                img.src = tour.poster;
+                imgdiv.appendChild(img);
+
+                let text = document.createElement("p");
+                text.innerHTML = `You chose ${tour.name} tour for ${tour.length}.
+                If you are age ${tour.age} and confirm your decision, 
+                fill the data and press SEND.
+                We will contact you ASAP!`;
+
+                let labelbox = document.createElement("div");
+
+                let name = document.createElement("input");
+                let letters = /^[A-Za-z]+$/;
+                let label = document.createElement("label");
+                label.innerHTML = "Enter your name:";
+                label.appendChild(name);
+                labelbox.appendChild(label);
+
+                let labelbox2 = document.createElement("div");
+                let phone = document.createElement("input");
+                phone.type = "tel";
+                phone.setAttribute("required", null);
+                let label2 = document.createElement("label");
+                label2.innerHTML = "Enter your phone number:";
+                label2.appendChild(phone);
+                labelbox2.appendChild(label2);
+
+                let btns = document.createElement("div");
+                let checkoutBtn = document.createElement("button");
+                checkoutBtn.innerHTML = `SEND`;
+                btns.appendChild(checkoutBtn);
+                let close = document.createElement("button");
+                close.innerHTML = `CLOSE`;
+                btns.appendChild(close);
+
+
+                checkoutInfo.appendChild(imgdiv);
+                checkoutInfo.appendChild(text);
+                checkoutInfo.appendChild(labelbox);
+                checkoutInfo.appendChild(labelbox2);
+                checkoutInfo.appendChild(btns);
+
+                box.appendChild(modalback);
+                box.appendChild(checkoutInfo);
+
+                checkoutBtn.addEventListener("click", () => {
+                    if (name.value === "" || !name.value.match(letters) || phone.value === "") {
+                        pushNotify("Something is wrong..", `You need to enter the correct data`, "error");
+
+                    } else {
+                        modalback.hidden = true;
+                        checkoutInfo.hidden = true;
+                        pushNotify("Success", `Thanks ${name.value}! We will contact you for details.`, "success");
+                    }
+                })
+
+                close.addEventListener("click", () => {
+                    checkout.hidden = false;
+                    modalback.hidden = true;
+                    checkoutInfo.hidden = true;
+                })
+            })
+        })
+        info.appendChild(box);
     });
+    document.querySelector("#choose").appendChild(info);
+
+});
+
+
+function pushNotify(title, text, type) {
+    new Notify({
+        status: type,
+        title: title,
+        text: text,
+        effect: 'fade',
+        speed: 300,
+        customClass: null,
+        customIcon: null,
+        isIcon: true,
+        isCloseButton: true,
+        autoclose: true,
+        autotimeout: 3000,
+        gap: 20,
+        distance: 20,
+        type: 1,
+        position: 'right top'
+    })
+}
+
+document.querySelector("form button").addEventListener("click", () => {
+    let name = document.querySelector("#name").value;
+    let email = document.querySelector("#email").value;
+    let emailValidator = /\S+@\S+\.\S+/;
+    let phone = document.querySelector("#phone").value;
+    let phoneValidator = /\(?\+[0-9]{1,3}\)? ?-?[0-9]{1,3} ?-?[0-9]{3,5} ?-?[0-9]{4}( ?-?[0-9]{3})? ?(\w{1,10}\s?\d{1,6})?/;
+    let checkbox = document.querySelector("#human");
+
+    if (emailValidator.test(email) === true && name !== "" && phoneValidator.test(phone) === true && checkbox.checked === true)  {
+        document.querySelector("form").reset();
+        pushNotify("Success", "We'll contact you", 'success')
+    } else pushNotify("Hmm.. Something went wrong", "Check if your info is correct", "error")
 
 })
