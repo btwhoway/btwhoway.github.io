59c73870e46460811f83e898b1df0901b646c0f4
Anna <btwhoway@gmail.com>
2023-01-30 20:04:20 +0200

    statistics added

---
+++ library/js/stats.js
@@ -0,0 +1,74 @@
+function countNumberOfBooks() {
+    let booksCount = {};
+
+    for (let card of cardsArray) {
+        if (!booksCount.hasOwnProperty(card.book)) {
+            booksCount[card.book] = 0;
+        }
+
+        booksCount[card.book]++;
+    }
+
+    booksCount = Object.entries(booksCount);
+
+    booksCount = booksCount.sort((a, b) => {
+        return b[1] - a[1]
+    })
+
+    return booksCount;
+}
+
+function countNumberOfVisitors() {
+    let visitorsCount = {};
+
+    for (let card of cardsArray) {
+        if (!visitorsCount.hasOwnProperty(card.visitor)) {
+            visitorsCount[card.visitor] = 0;
+        }
+
+        visitorsCount[card.visitor]++;
+    }
+
+    visitorsCount = Object.entries(visitorsCount);
+
+    visitorsCount = visitorsCount.sort((a, b) => {
+        return b[1] - a[1]
+    })
+
+    return visitorsCount;
+
+}
+
+function showPopular(booksCount) {
+    let div = document.querySelector(".popular-books");
+
+    for (let i = 0; i < 5; i++) {
+        let book = booksCount[i];
+        let p = document.createElement("p");
+        p.textContent = `${book[0]}: ${book[1]}`;
+        p.style.textTransform = "capitalize";
+
+        div.appendChild(p);
+    }
+
+}
+
+let booksCount = countNumberOfBooks();
+showPopular(booksCount);
+
+function showActiveVisitors(visitorsCount) {
+    let div2 = document.querySelector(".active-visitors");
+
+    for (let i = 0; i < 5; i++) {
+        let visitor = visitorsCount[i]
+        let p = document.createElement("p");
+        p.style.textTransform = "capitalize";
+
+        p.textContent = `${visitor[0]}: ${visitor[1]}`;
+        div2.appendChild(p);
+    }
+}
+
+let visitorsCount = countNumberOfVisitors();
+showActiveVisitors(visitorsCount);
+
